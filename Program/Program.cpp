7745542edaf802925e7cc256d6cd97625ff37542
dudrldudwls12 <cacasi376@gmail.com>
2024-07-11 19:47:23 +0900

    (2024.7.11) 브루트 포스

---
+++ Program/Program.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool IsPrime(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	cout << IsPrime(5) << endl;
+	cout << IsPrime(10) << endl;
+	return 0;
+}
