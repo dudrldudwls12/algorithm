345c0cd7a49b5263ceb52cc5113085b267c9963d
dudrldudwls12 <cacasi376@gmail.com>
2024-07-12 19:47:22 +0900

    (2024.7.12)

---
+++ Class2th (Sieve of Eratosthenes)/Source.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int list[101];
+
+bool IsPrime(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void Sieve(int n)
+{
+	for (int i = 0; i <= n; i++)
+	{
+		list[i] = i;
+	}
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (list[i] == 0)
+		{
+			continue;
+		}
+		for (int j = i * 2; j <= n; j += i)
+		{
+			list[j] = 0;
+		}
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		if (list[i] != 0)
+		{
+			cout << list[i] << "  ";
+		}
+	}
+}
+
+int main()
+{
+	// cout << IsPrime(5) << endl;
+	// cout << IsPrime(10) << endl;
+
+	Sieve(49);
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,26 +1,32 @@
 ﻿#include <iostream>
 
+
+
 using namespace std;
 
-bool IsPrime(int n)
-{
-	if (n <= 1)
-	{
-		return false;
-	}
-	for (int i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			return false;
-		}
-	}
-	return true;
-}
+#define SIZE 5
 
 int main()
 {
-	cout << IsPrime(5) << endl;
-	cout << IsPrime(10) << endl;
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘입니다.
+
+	int list[SIZE] = { 13, 8, 97, 1, 36 };
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; (SIZE - i) - 1; j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				std::swap(list[j], list[j + 1]);
+			}
+		}
+	}
+	for (const int& element : list)
+	{
+		cout << element << "  ";
+	}
+#pragma endregion
+
 	return 0;
 }
