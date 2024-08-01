7232e452ea377284504ca23479b86c810673b284
dudrldudwls12 <cacasi376@gmail.com>
2024-08-01 19:45:23 +0900

    (2024.8.1) 다익스트라 알고리즘

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,25 +2,52 @@
 
 using namespace std;
 
-#define SIZE 5
+#define SIZE 6
+#define INFINITY 1000000
 
+class Graph
+{
+private:
+	int weghit[SIZE][SIZE] =
+	{
+		{0, 2, 5, 3, INFINITY, INFINITY},
+		{2, 0, 7, INFINITY, INFINITY, 9},
+		{5, 7, 0, 1, 5, 2},
+		{3, INFINITY, 1, 0, 7, INFINITY},
+		{INFINITY, INFINITY, 2, 7, 0, 2},
+		{INFINITY, 9, 5, INFINITY, 2, 0},
+	};
+	bool visited[SIZE];
+	int distance[SIZE];
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = 0;
+		}
+	}
+};
 int main()
 {
-#pragma region 누적합
-	// 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다.
-	
-	int list[SIZE] = { 1,2,3,4,5 };
-	int sum[SIZE] = { 1,0,0,0,0 };
+#pragma region 다익스트라 알고리즘
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는
+	// 알고리즘 입니다.
 
-	sum[0] = list[0];
-	for (int i = 1; i < SIZE; i++)
-	{
-		sum[i] = sum[i - 1] + list[i];
-	}
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << sum[i] << "  ";
-	}
+	// 1. distance 배열에 weiht[시작점 노드]의 값들로
+	//     초기화합니다.
+
+	// 2. 시작점을 방문 처리합니다.
+	
+	// 3. ditance 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	//     단, 이미 방문한 노드는 제외합니다.
+
+	// 4. 최소 비용 노드를 가져갈 지 고민해서 distance 배열을 갱신합니다.
+	//     단, 이미 방문한 노드는 제외합니다.
+
+	// 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합닌다.
+
+	// 6. 
 #pragma endregion
 
 	return 0;
